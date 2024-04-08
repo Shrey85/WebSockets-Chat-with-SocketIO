8b165c37ddb6adec06b69b281902928646540252
Shrey85 <157231201+Shrey85@users.noreply.github.com>
2024-04-07 22:28:55 -0400

    second commit

---
+++ ecosystem.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    apps : [{
+      name: 'WebSockets-Chat-with-SocketIO',
+      script: 'index.js', // or the entry point of your application
+      // other configuration options
+    }]
+  };
