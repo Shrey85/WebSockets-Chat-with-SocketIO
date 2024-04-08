d41ceeae0f34cb4e1d512b36a0d5f15f5b818ca0
Shrey85 <157231201+Shrey85@users.noreply.github.com>
2024-04-07 22:17:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ io.on('connection', (socket) => {
 });
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log('listening on *:3002');
 });
 
 io.on('connection', (socket) => {
